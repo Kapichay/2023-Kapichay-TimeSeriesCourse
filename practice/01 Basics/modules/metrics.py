99f0f273ea7983afff3cf201eb4ecf43073e83a1
Kapichay <113962495+Kapichay@users.noreply.github.com>
2023-09-22 13:02:46 +0500

    01 Basics

--- practice/01 Basics/modules/metrics.py
+++ practice/01 Basics/modules/metrics.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import math
+from numpy. linalg import norm
+import pandas as pd
 
 def ED_distance(ts1, ts2):
     """
@@ -18,12 +20,13 @@ def ED_distance(ts1, ts2):
     ed_dist : float
         Euclidean distance between ts1 and ts2.
     """
-    
-    ed_dist = 0
 
-    # INSERT YOUR CODE
-    
-    return ed_dist
+    ED = []
+
+    for i in range(len(ts1)):
+        ED.append((ts1[i] - ts2[i]) ** 2)
+
+    return math.sqrt(sum(ED))
 
 
 def norm_ED_distance(ts1, ts2):
@@ -72,8 +75,24 @@ def DTW_distance(ts1, ts2, r=None):
         DTW distance between ts1 and ts2.
     """
 
-    dtw_dist = 0
+    n = len(ts1)
+    m = len(ts2)
 
-    # INSERT YOUR CODE
+    # Создаем матрицу для хранения вычисленных расстояний
+    dp = np.zeros((n, m))
 
-    return dtw_dist
+    # Заполняем первую строку и первый столбец матрицы
+    for i in range(n):
+        for j in range(m):
+            d = abs(ts1[i] - ts2[j]) ** 2
+            if i == 0 and j == 0:
+                dp[i, j] = d
+            elif i == 0:
+                dp[i, j] = d + dp[i, j - 1]
+            elif j == 0:
+                dp[i, j] = d + dp[i - 1, j]
+            else:
+                dp[i, j] = d + min(dp[i - 1, j], dp[i, j - 1], dp[i - 1, j - 1])
+
+    # Возвращаем значение DTW расстояния между последними элементами
+    return dp[n - 1, m - 1]
