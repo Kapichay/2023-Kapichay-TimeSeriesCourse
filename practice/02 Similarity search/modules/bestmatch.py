2368282103af395adac6c38408a28e758c9556c4
Kapichay <kapichai911@gmail.com>
2023-10-15 18:00:06 +0500

    work 2 part 2

--- practice/02 Similarity search/modules/bestmatch.py
+++ practice/02 Similarity search/modules/bestmatch.py
@@ -186,8 +186,8 @@ class UCR_DTW(BestMatchFinder):
 
         lb_Kim = 0
 
-        # INSERT YOUR CODE
-        
+        lb_Kim = np.sqrt((subs1[0] - subs2[0]) ** 2) + np.sqrt((subs1[-1] - subs2[-1]) ** 2)
+                
         return lb_Kim
 
 
@@ -214,7 +214,17 @@ class UCR_DTW(BestMatchFinder):
         
         lb_Keogh = 0
 
-        # INSERT YOUR CODE
+        res = []
+
+        for i in range(len(subs1)):
+            if subs2[i] > max(subs1[max(0, i-r):min(len(subs1), i+r+1)]):
+                res.append((subs2[i] - max(subs1[max(0, i-r):min(len(subs1), i+r+1)])) ** 2)
+            elif subs2[i] < min(subs1[max(0, i-r):min(len(subs1), i+r+1)]):
+                res.append((subs2[i] - min(subs1[max(0, i-r):min(len(subs1), i+r+1)])) ** 2)
+            else:
+                res.append(0)
+        
+        lb_Keogh = sum(res)
 
         return lb_Keogh
 
@@ -241,7 +251,32 @@ class UCR_DTW(BestMatchFinder):
         self.lb_KeoghQC_num = 0
         self.lb_KeoghCQ_num = 0
         
-        # INSERT YOUR CODE
+        distances = []
+        
+        for subseq_idx in range(N):
+            subseq = self.ts_data[subseq_idx]
+            
+            if self.normalize:
+                subseq = z_normalize(subseq)
+                self.query = z_normalize(self.query)
+
+            if self._LB_Kim(self.query, subseq) > bsf:
+                self.lb_Kim_num += 1
+
+            elif self._LB_Keogh(self.query, subseq, int(self.r*m)) > bsf:
+                self.lb_KeoghQC_num += 1
+
+            elif self._LB_Keogh(subseq, self.query, int(self.r*m)) > bsf:
+                self.lb_KeoghCQ_num += 1
+
+            else:
+              dist = DTW_distance(self.query, subseq, self.r)
+              distances.append(dist)
+              if dist < bsf:
+                  bsf = dist
+
+
+        self.bestmatch = self._top_k_match(distances, m, bsf, excl_zone)
 
         return {'index' : self.bestmatch['index'],
                 'distance' : self.bestmatch['distance'],
